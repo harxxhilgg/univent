23b88af48a23058de74b9c84cd779ba2e1a985c8
Harshil Patel <harshilp046@gmail.com>
2025-02-28 02:04:11 +0530

    feat: Accept header in axios instance

--- univent-backend/src/utils/api.ts
+++ univent-backend/src/utils/api.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 
-export const API_URL = "http://192.168.195.200:5000/api";
+export const API_URL = "http://192.168.31.14:5000/api";
 
 export const api = axios.create({
   baseURL: API_URL,
   headers: {
     "Content-Type": "application/json",
+    Accept: "application/json",
   },
 });
