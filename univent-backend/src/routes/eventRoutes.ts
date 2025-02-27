a2dad80540b7a10e10693f134acd2677bdf65a62
Harshil Patel <harshilp046@gmail.com>
2025-02-28 02:04:30 +0530

    feat: add image upload route using multer in eventRoutes

--- univent-backend/src/routes/eventRoutes.ts
+++ univent-backend/src/routes/eventRoutes.ts
@@ -1,16 +1,24 @@
 import express from "express";
+import multer from "multer";
 import {
   createEvent,
   getAllEvents,
   getEventsByUser,
+  uploadImage,
 } from "../controllers/eventController";
 
 const router = express.Router();
 
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+router.post(
+  "/upload",
+  upload.single("image"),
+  uploadImage as express.RequestHandler
+);
 router.post("/create", createEvent as any);
-
 router.get("/getAllEvents", getAllEvents);
-
 router.get("/user/:email", getEventsByUser);
 
 export default router;
