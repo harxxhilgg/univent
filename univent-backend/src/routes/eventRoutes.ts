5668af70da3124d7f8c02563fb1ef445e49e03fa
Harshil Patel <harshilp046@gmail.com>
2025-04-28 16:24:38 +0530

    refactor: reorder event route handlers for better organization

--- univent-backend/src/routes/eventRoutes.ts
+++ univent-backend/src/routes/eventRoutes.ts
@@ -2,17 +2,23 @@ import express from "express";
 import multer from "multer";
 import {
   createEvent,
-  getAllEvents,
+  getUpcomingEvents,
   getEventsByUser,
   uploadImage,
   search,
   getLatestEvent,
+  getCurrentEvents,
+  getAllEvents,
 } from "../controllers/eventController";
 
 const router = express.Router();
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
+
+router.get("/", (req, res) => {
+  res.json({ message: "Event route is working." });
+});
 
 router.post(
   "/upload",
@@ -21,8 +27,10 @@ router.post(
 );
 router.post("/create", createEvent as any);
 router.get("/getAllEvents", getAllEvents);
+router.get("/getUpcomingEvents", getUpcomingEvents);
 router.get("/user/:email", getEventsByUser);
 router.get("/search", search);
 router.get("/getLatestEvent", getLatestEvent);
+router.get("/getCurrentEvents", getCurrentEvents);
 
 export default router;
