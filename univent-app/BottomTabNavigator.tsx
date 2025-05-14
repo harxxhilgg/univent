9859eedcc3173adec11cb93c5dc79bf89442c50e
Harshil Patel <harshilp046@gmail.com>
2025-05-14 13:58:10 +0530

    feat: update BottomTabNavigator with new icons, adjust animation timings, and refine styles

--- univent-app/BottomTabNavigator.tsx
+++ univent-app/BottomTabNavigator.tsx
@@ -1,9 +1,8 @@
 import { StyleSheet, View, Platform, Dimensions, Keyboard, UIManager, Animated } from 'react-native';
 import React, { useEffect, useRef } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
-import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { theme } from './theme';
+import { FontAwesome, Fontisto, Ionicons, Octicons } from '@expo/vector-icons';
 
 // screens
 import UniventHome from './src/screens/UniventHome';
@@ -11,7 +10,6 @@ import MyEvents from './src/screens/MyEvents';
 import CreateEvent from './src/screens/CreateEvent';
 import Updates from './src/screens/Updates';
 import Settings from './src/screens/Settings';
-import { theme } from './theme';
 
 const Tab = createBottomTabNavigator();
 
@@ -23,19 +21,13 @@ export default function BottomTabNavigator() {
   const { width } = Dimensions.get('window');
 
   const translateY = useRef(new Animated.Value(0)).current;
-  const opacity = useRef(new Animated.Value(1)).current;
 
   useEffect(() => {
     const keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', () => {
       Animated.parallel([
         Animated.timing(translateY, {
-          toValue: 100,
-          duration: 200,
-          useNativeDriver: true
-        }),
-        Animated.timing(opacity, {
-          toValue: 0,
-          duration: 200,
+          toValue: 250,
+          duration: 300,
           useNativeDriver: true
         }),
       ]).start();
@@ -44,12 +36,7 @@ export default function BottomTabNavigator() {
       Animated.parallel([
         Animated.timing(translateY, {
           toValue: 0,
-          duration: 200,
-          useNativeDriver: true
-        }),
-        Animated.timing(opacity, {
-          toValue: 1,
-          duration: 200,
+          duration: 500,
           useNativeDriver: true
         }),
       ]).start();
@@ -59,58 +46,59 @@ export default function BottomTabNavigator() {
       keyboardDidShowListener.remove();
       keyboardDidHideListener.remove();
     }
-  }, [opacity, translateY]);
+  }, [translateY]);
 
   return (
     <Tab.Navigator
       screenOptions={{
         headerStyle: {
-          backgroundColor: theme.colorBackgroundDark,
+          backgroundColor: theme.colorBackgroundDark
         },
-        headerTintColor: theme.colorTaskbarYellow,
+        headerTintColor: theme.colorTabBarTint,
         tabBarShowLabel: false,
+        tabBarPosition: "bottom",
         tabBarStyle: {
           transform: [{ translateY }],
-          opacity,
           position: "absolute",
-          bottom: 0,
-          height: 80,
           backgroundColor: theme.colorSlightDark,
-          paddingTop: Platform.OS === 'web' ? 0 : 20,
-          marginHorizontal: Platform.OS === 'web' ? 0 : width > 1000 ? (width - 500) / 2 : 22,
-          marginBottom: 28,
-          borderRadius: 48,
+          height: 80,
+          marginHorizontal: Platform.OS === 'web' ? 0 : width > 1000 ? (width - 500) / 2 : 10,
+          paddingTop: 20,
+          marginBottom: 4,
+          borderRadius: 98,
           overflow: "hidden",
           borderColor: "transparent",
-          elevation: 1000,
-          boxShadow: "0px 0px 60px #000000",
+          boxShadow: "0px 0px 60px #000000"
         },
-        tabBarActiveTintColor: theme.colorTaskbarYellow,
+        tabBarItemStyle: {
+          height: 64,
+          justifyContent: "center",
+          alignItems: "center"
+        },
+        tabBarActiveTintColor: theme.colorTabBarTint,
         tabBarInactiveTintColor: theme.colorTintInactive,
       }}
     >
 
       {/* Discover Tab */}
-
       <Tab.Screen
         name="Univent"
         component={UniventHome}
         options={{
           headerTitle: 'Univent',
-          headerTitleAlign: 'center',
           headerTitleStyle: {
-            fontSize: 22,
-            marginVertical: 15,
-            fontWeight: 'bold'
+            fontFamily: "DreamAvenue",
+            fontSize: 36,
+            marginLeft: 4,
+            color: theme.colorTabBarTint
           },
           tabBarIcon: ({ color, size }) => (
-            <FontAwesome6 name="globe" size={size} color={color} />
+            <Octicons name="home" size={size} color={color} />
           ),
         }}
       />
 
       {/* My-Events Tab */}
-
       <Tab.Screen
         name="My Events"
         component={MyEvents}
@@ -123,34 +111,32 @@ export default function BottomTabNavigator() {
             fontWeight: 'bold',
           },
           tabBarIcon: ({ color, size }) => (
-            <MaterialIcons name="event" size={(size + 2)} color={color} />
+            <Ionicons name="calendar-outline" size={(size + 1)} color={color} />
           )
         }}
       />
 
       {/* createEvent */}
-
       <Tab.Screen
         name="CreateEvent"
         component={CreateEvent}
         options={{
-          headerTitle: 'Create event',
+          headerTitle: 'Create Event',
           headerTitleAlign: 'center',
           headerTitleStyle: {
             fontSize: 24,
             marginVertical: 15,
-            fontWeight: 'bold',
+            fontWeight: 'bold'
           },
           tabBarIcon: ({ color, size }) => (
             <View style={styles.customTabButton}>
-              <FontAwesome5 name="plus" size={20} color={theme.colorBackgroundDark} />
+              <Fontisto name="plus-a" size={20} color={theme.colorBackgroundDark} />
             </View>
           )
         }}
       />
 
       {/* Updates Tab */}
-
       <Tab.Screen
         name="Updates"
         component={Updates}
@@ -163,7 +149,7 @@ export default function BottomTabNavigator() {
             fontWeight: 'bold',
           },
           tabBarIcon: ({ color, size }) => (
-            <MaterialIcons name="tips-and-updates" size={size} color={color} />
+            <Ionicons name="bulb-outline" size={(size + 1)} color={color} />
           ),
         }}
       />
@@ -182,7 +168,7 @@ export default function BottomTabNavigator() {
             fontWeight: 'bold',
           },
           tabBarIcon: ({ color, size }) => (
-            <FontAwesome5 name="cog" size={size} color={color} />
+            <FontAwesome name="user-o" size={size} color={color} />
           ),
         }}
       />
@@ -194,9 +180,9 @@ const styles = StyleSheet.create({
   customTabButton: {
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: theme.colorTaskbarYellow,
-    width: 60,
-    height: 60,
-    borderRadius: 48,
-  },
+    backgroundColor: theme.colorTabBarTint,
+    width: 55,
+    height: 55,
+    borderRadius: 98
+  }
 });
