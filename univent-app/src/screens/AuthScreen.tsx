a071c3f100f460e079deb9728bd2f32cc4a0c3d7
Harshil Patel <harshilp046@gmail.com>
2025-05-14 14:04:38 +0530

    refactor: enhance AuthScreen layout and improve error handling messages

--- univent-app/src/screens/AuthScreen.tsx
+++ univent-app/src/screens/AuthScreen.tsx
@@ -1,16 +1,16 @@
-import { View, TouchableOpacity, Image, StyleSheet, TouchableWithoutFeedback, Keyboard, ScrollView, KeyboardAvoidingView, Platform, ActivityIndicator } from 'react-native';
+import { View, TouchableOpacity, StyleSheet, TouchableWithoutFeedback, Keyboard, ScrollView, KeyboardAvoidingView, Platform, ActivityIndicator, Text } from 'react-native';
 import CustomText from '../components/CustomText';
 import { theme } from '../../theme';
 import { useContext, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { AuthScreenNavigationProp } from '../../App';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import Toast from 'react-native-toast-message';
 import { API_URL } from '../utils/api';
 import { UserContext } from '../context/UserContext';
 import { TextInput as TextInputPaper } from 'react-native-paper';
 import { decodeJwtPayload } from '../context/UserProvider';
 import { useToast } from '../components/useToast';
+import { LinearGradient } from 'expo-linear-gradient';
 
 const AuthScreen = () => {
   const { setUser } = useContext(UserContext);
@@ -54,7 +54,7 @@ const AuthScreen = () => {
 
       if (!response.ok) {
         console.log('Login failed: ', data);
-        showError(3000, 'Login failed, please try again.');
+        showError(3000, 'Login failed', 'Please try again.');
         setLoginLoading(false);
         return;
       };
@@ -70,7 +70,7 @@ const AuthScreen = () => {
         });
       } catch (storageError) {
         console.log('Error storing token: ', storageError);
-        showError(2500, 'Something went wrong, please try again.');
+        showError(2500, 'Something went wrong', 'please try again.');
         setLoginLoading(false);
         return;
       };
@@ -98,29 +98,8 @@ const AuthScreen = () => {
     };
   };
 
-  const showToastSomethingIsWrong = () => {
-    Toast.show({
-      autoHide: true,
-      visibilityTime: 3000,
-      type: 'error',
-      text1: 'Something is wrong with the app!',
-      text2: 'Please restart the app.',
-    });
-  };
-
-  const showToastGuestLoginSuccess = () => {
-    Toast.show({
-      autoHide: true,
-      visibilityTime: 1500,
-      type: 'success',
-      text1: 'Logged in as a guest!'
-    });
-  };
-
   const handleGuestLogin = async () => {
-
     setGuestLoading(true);
-
     try {
       // setting up guest data
       const data = {
@@ -129,20 +108,18 @@ const AuthScreen = () => {
           email: "user.guest@univent.com"
         },
       };
-
       setUser(data.user);
-
       try {
         navigation.replace("Main");
-        showToastGuestLoginSuccess();
+        showSuccess(1500, 'Logged in as a guest!');
         setGuestLoading(false);
       } catch (err) {
         console.error(err);
-        showToastSomethingIsWrong();
+        showError(2500, "Something is wrong with the app", "Please restart the app");
       }
     } catch (err) {
       console.error(err);
-      showToastSomethingIsWrong();
+      showError(2500, "Something is wrong with the app", "Please restart the app");
     } finally {
       setGuestLoading(false);
     }
@@ -158,10 +135,12 @@ const AuthScreen = () => {
           contentContainerStyle={styles.scrollContainer}
           keyboardShouldPersistTaps="handled"
         >
-          <Image
-            source={require('../../assets/logos/full-logo.png')}
-            style={styles.logo}
-          />
+          <View style={styles.logoContainer}>
+            <Text style={styles.logo}>Univent</Text>
+          </View>
+          <View style={styles.grettingContainer}>
+            <CustomText style={styles.grettingText}>Login to your Account</CustomText>
+          </View>
           <View style={styles.inputContainer}>
             <TextInputPaper
               keyboardType="email-address"
@@ -173,9 +152,9 @@ const AuthScreen = () => {
               onBlur={() => setIsFocused(false)}
               style={styles.input}
               mode="outlined"
-              theme={{ colors: { primary: theme.colorTaskbarYellow, background: theme.colorBackgroundDark } }}
+              theme={{ colors: { primary: theme.colorWhite, background: theme.colorBackgroundDark } }}
               textColor={theme.colorFontLight}
-              outlineStyle={{ borderRadius: 14 }}
+              outlineStyle={{ borderRadius: 10 }}
             />
             <TextInputPaper
               keyboardType="default"
@@ -187,9 +166,9 @@ const AuthScreen = () => {
               onBlur={() => setIsFocused(false)}
               style={styles.input}
               mode="outlined"
-              theme={{ colors: { primary: theme.colorTaskbarYellow, background: theme.colorBackgroundDark } }}
+              theme={{ colors: { primary: theme.colorWhite, background: theme.colorBackgroundDark } }}
               textColor={theme.colorFontLight}
-              outlineStyle={{ borderRadius: 14 }}
+              outlineStyle={{ borderRadius: 10 }}
               secureTextEntry={secureTextEntry}
               right={
                 <TextInputPaper.Icon
@@ -199,29 +178,52 @@ const AuthScreen = () => {
               }
             />
           </View>
-          <TouchableOpacity style={styles.loginBtn} onPress={handleLogin} disabled={loginLoading}>
-            {loginLoading ? (
-              <ActivityIndicator color={theme.colorFontDark} style={styles.activityIndicator} />
-            ) : (
-              <CustomText style={styles.loginBtnText}>Log in</CustomText>
-            )}
+          <TouchableOpacity onPress={handleLogin} disabled={loginLoading} style={styles.loginBtnContainer}>
+            <LinearGradient
+              colors={['rgb(210, 238, 255)', 'rgb(250, 250, 250)', 'rgb(210, 238, 255)']}
+              start={{ x: 0, y: 1 }}
+              end={{ x: 1, y: 0 }}
+              style={styles.gradientBackground}
+            >
+              {loginLoading ? (
+                <ActivityIndicator color={theme.colorFontDark} style={styles.activityIndicator} />
+              ) : (
+                <CustomText style={styles.loginBtnText} semibold>Log in</CustomText>
+              )}
+            </LinearGradient>
           </TouchableOpacity>
 
-          <TouchableOpacity style={styles.guestLoginBtn} onPress={handleGuestLogin} disabled={GuestLoading}>
-            {GuestLoading ? (
-              <ActivityIndicator color={theme.colorFontLight} style={styles.activityIndicator} />
-            ) : (
-              <CustomText style={styles.guestLoginBtnText}>Guest Login</CustomText>
-            )}
+          <TouchableOpacity onPress={handleGuestLogin} disabled={GuestLoading} style={styles.loginBtnContainer}>
+            <LinearGradient
+              colors={['rgb(210, 255, 238)', 'rgb(255, 255, 255)', 'rgb(210, 255, 238)']}
+              start={{ x: 0, y: 1 }}
+              end={{ x: 1, y: 0 }}
+              style={styles.gradientBackground}
+            >
+              {GuestLoading ? (
+                <ActivityIndicator color={theme.colorFontDark} style={styles.activityIndicator} />
+              ) : (
+                <CustomText style={styles.guestLoginBtnText} semibold>Guest Login</CustomText>
+              )}
+            </LinearGradient>
           </TouchableOpacity>
-
           <View style={styles.newAccContainer}>
-            <TouchableOpacity style={styles.signUpBtn} onPress={() => {
-              navigation.navigate('Signup');
-              setGuestLoading(true);
-            }}>
-              <CustomText style={styles.SignupBtnText}>Create new account</CustomText>
+            <TouchableOpacity
+              onPress={() => {
+                navigation.replace('Signup');
+                setGuestLoading(true);
+              }}
+            >
+              <LinearGradient
+                colors={['rgb(250, 250, 250)', 'rgb(210, 238, 255)', 'rgb(250, 250, 250)']}
+                start={{ x: 0, y: 1 }}
+                end={{ x: 1, y: 0 }}
+                style={styles.gradientBackground}
+              >
+                <CustomText style={styles.SignupBtnText} semibold>Create new Account</CustomText>
+              </LinearGradient>
             </TouchableOpacity>
+            <CustomText style={styles.alreadyUserText}>New User?</CustomText>
           </View>
         </ScrollView>
       </TouchableWithoutFeedback>
@@ -236,71 +238,83 @@ const styles = StyleSheet.create({
   scrollContainer: {
     flexGrow: 1,
     alignItems: "center",
-    backgroundColor: theme.colorBackgroundDark,
-    paddingBottom: 30,
+    backgroundColor: theme.colorBackgroundDark
+  },
+  logoContainer: {
+    marginTop: 110,
+    marginBottom: 90,
+    width: "90%",
+    alignItems: "center",
+    maxWidth: 500
   },
   logo: {
-    width: 400,
-    height: 400,
-    resizeMode: "contain",
-    marginTop: -40,
-    marginBottom: -10
-  },
-  input: {
-    fontSize: 16,
+    fontFamily: "DreamAvenue",
+    fontSize: 60,
     color: theme.colorFontLight,
+    userSelect: "none"
+  },
+  grettingContainer: {
+    marginBottom: 14,
+    width: "89%",
+    maxWidth: 500
+  },
+  grettingText: {
+    fontFamily: "ZenOldMincho",
+    fontSize: 22,
+    color: theme.colorFontLight
   },
   inputContainer: {
-    width: "85%",
-    gap: 12,
+    width: "90%",
+    maxWidth: 500,
+    gap: 6,
+    marginBottom: 12
+  },
+  input: {
+    fontSize: 15
   },
   activityIndicator: {
-    paddingVertical: 3,
+    paddingVertical: 3
   },
-  loginBtn: {
-    marginTop: 14,
-    backgroundColor: theme.colorTaskbarYellow,
-    paddingVertical: 6,
-    width: "85%",
-    borderRadius: 20
+  loginBtnContainer: {
+    width: "90%",
+    maxWidth: 500,
+    marginBottom: 8
+  },
+  gradientBackground: {
+    paddingVertical: 8,
+    borderRadius: 10,
+    overflow: "hidden",
+    alignItems: "center"
   },
   loginBtnText: {
     color: theme.colorFontDark,
-    textAlign: "center",
-    letterSpacing: 1,
-    fontWeight: '700'
-  },
-  guestLoginBtn: {
-    marginVertical: 14,
-    backgroundColor: theme.colorSlightDark,
-    paddingVertical: 6,
-    width: "85%",
-    borderRadius: 20
+    letterSpacing: 0.5,
+    fontSize: 15
   },
   guestLoginBtnText: {
-    color: theme.colorTaskbarYellow,
-    textAlign: "center",
-    letterSpacing: 1,
-    fontWeight: '700'
+    color: theme.colorFontDark,
+    letterSpacing: 0.5,
+    fontSize: 15
   },
   newAccContainer: {
     flex: 1,
     flexDirection: "column-reverse",
-    marginBottom: 20,
-    width: "85%",
-  },
-  signUpBtn: {
-    paddingVertical: 6,
-    borderWidth: 1,
-    borderColor: theme.colorTaskbarYellow,
-    borderRadius: 20
+    marginBottom: 10,
+    width: "90%",
+    maxWidth: 500
   },
   SignupBtnText: {
-    color: theme.colorTaskbarYellow,
-    textAlign: "center",
-    letterSpacing: 1,
-    fontWeight: '700'
+    color: theme.colorFontDark,
+    letterSpacing: 0.5,
+    fontSize: 15
   },
+  alreadyUserText: {
+    color: theme.colorFontGray,
+    fontSize: 13,
+    textAlign: "center",
+    marginBottom: 5,
+    letterSpacing: 0.5
+  }
 });
 
 export default AuthScreen;
