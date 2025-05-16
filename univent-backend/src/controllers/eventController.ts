e01bcb0a38767db44e2a8864c0d2175899669f5c
Harshil Patel <harshilp046@gmail.com>
2025-05-16 21:44:20 +0530

    refactor: include additional fields in getLatestEvent query for comprehensive event details

--- univent-backend/src/controllers/eventController.ts
+++ univent-backend/src/controllers/eventController.ts
@@ -271,9 +271,15 @@ export const getLatestEvent = async (req: Request, res: Response) => {
     const result = await pool.query(
       `
       SELECT
+        id,
         title,
+        organizer,
         event_date::TEXT AS event_date, -- ensuring returned as plain text, no time conversions.
-        event_time
+        event_time,
+        location,
+        image_url,
+        is_paid,
+        created_by_email
       FROM
         events
       WHERE
