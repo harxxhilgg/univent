daae68d358b7b88e5f6556d08fb90f7994b33e8c
Harshil Patel <harshilp046@gmail.com>
2025-06-06 22:01:46 +0530

    refactor: remove duplicate import of forgotPassword in authRoutes

--- univent-backend/src/routes/authRoutes.ts
+++ univent-backend/src/routes/authRoutes.ts
@@ -4,6 +4,7 @@ import {
   login,
   deleteAccount,
   updateProfile,
+  forgotPassword,
 } from "../controllers/authController";
 import { validateSignup, validateLogin } from "../middlewares/authValiation";
 
@@ -17,5 +18,6 @@ router.post("/login", validateLogin as any, login as any);
 router.post("/signup", validateSignup as any, signup as any);
 router.delete("/deleteAccount", deleteAccount as any);
 router.put("/updateProfile", updateProfile as any);
+router.post("/forgotPassword", forgotPassword as any);
 
 export default router;
