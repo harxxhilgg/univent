f2d4da8f480c4158af71d33a5b3a34b1f1710be6
Harshil Patel <harshilp046@gmail.com>
2025-06-10 15:45:19 +0530

    test: add unit tests for CustomText component

---
+++ univent-app/src/components/__tests__/CustomText.test.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import CustomText from '../CustomText';
+
+describe('CustomText Component', () => {
+  it('renders correctly with default props', () => {
+    const { getByText } = render(<CustomText>Helloww World</CustomText>);
+    const text = getByText('Helloww World');
+
+    expect(text).toBeTruthy();
+    expect(text.props.style).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ fontSize: 16, lineHeight: 26 }),
+        expect.objectContaining({ fontFamily: 'Inter-Regular' }),
+      ])
+    );
+  });
+
+  it('applies Inter-Bold when bold prop is true', () => {
+    const { getByText } = render(<CustomText bold>Bold Text</CustomText>);
+    const text = getByText('Bold Text');
+
+    expect(text.props.style).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ fontFamily: 'Inter-Bold' }),
+      ])
+    );
+  });
+
+  it('applies Inter-SemiBold  when semibold prop is true', () => {
+    const { getByText } = render(<CustomText semibold>SemiBold Text</CustomText>);
+    const text = getByText('SemiBold Text');
+
+    expect(text.props.style).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ fontFamily: 'Inter-SemiBold' }),
+      ])
+    );
+  });
+
+  it('applies custom styles passed via style prop', () => {
+    const { getByText } = render(
+      <CustomText style={{ color: 'purple' }}>Styled Text</CustomText>
+    );
+    const text = getByText('Styled Text');
+
+    expect(text.props.style).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ color: 'purple' }),
+      ])
+    );
+  });
+})
