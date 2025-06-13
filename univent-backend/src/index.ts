ad9d04f1038fe2aad61b1739d4bbea964f7dac7d
Harshil Patel <harshilp046@gmail.com>
2025-06-13 22:23:44 +0530

    feat: add default routes to the API for enhanced routing capabilities

--- univent-backend/src/index.ts
+++ univent-backend/src/index.ts
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import "./config/db";
 import authRoutes from "./routes/authRoutes";
 import eventRoutes from "./routes/eventRoutes";
+import defaultRoutes from "./routes/defaultRoutes";
 
 dotenv.config();
 
@@ -31,6 +32,7 @@ app.get("/api", (req, res) => {
 
 app.use("/api/auth", authRoutes);
 app.use("/api/events", eventRoutes);
+app.use("/api/default", defaultRoutes);
 
 app.listen(PORT, () => {
   console.log("Server running on port:", PORT);
