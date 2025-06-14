14abb8ad8417a48005ee037110e262ba4409b2ec
Harshil Patel <harshilp046@gmail.com>
2025-06-14 22:11:36 +0530

    refactor: remove expo-modules-autolinking and expo-application from plugins in app.config.js

--- univent-app/app.config.js
+++ univent-app/app.config.js
@@ -53,9 +53,7 @@ export default {
           resizeMode: "contain",
           backgroundColor: "#080b12"
         }
-      ],
-      "expo-modules-autolinking",
-      "expo-application"
+      ]
     ],
     extra: {
       API_URL: process.env.API_URL,
