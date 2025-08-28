711ccbcf4be604bc728a3d761d0ffcca7928d81e
Harshil Patel <harshilp046@gmail.com>
2025-08-28 23:32:51 +0530

    fix: add missing comma in theme object definition

--- univent-app/theme.ts
+++ univent-app/theme.ts
@@ -22,5 +22,5 @@ export const theme = {
   colorExclusiveYellow: "#FFED29",
   colorButtonGray: "#595959",
   colorTabBarTint: "#FBFFFF",
-  colorBottomSheetDark: "#10141C"
+  colorBottomSheetDark: "#10141C",
 };
